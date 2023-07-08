d3f90409f25b8c72325a437f468f1e14d927559b
M.Anas Khan <a4anas406@gmail.com>
2023-07-09 01:20:17 +0500

    Conversation

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCaW1_YD5AOrdXZCJFMJ6DPf6bs3w3fhpQ",
-    authDomain: "conversation-app-3d804.firebaseapp.com",
-    projectId: "conversation-app-3d804",
-    storageBucket: "conversation-app-3d804.appspot.com",
-    messagingSenderId: "768186841488",
-    appId: "1:768186841488:web:b452ddf681b75ee34a9b15",
-    measurementId: "G-RRR5QKQKMF"
-  };
-
-
-// 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCaW1_YD5AOrdXZCJFMJ6DPf6bs3w3fhpQ",
+//     authDomain: "conversation-app-3d804.firebaseapp.com",
+//     projectId: "conversation-app-3d804",
+//     storageBucket: "conversation-app-3d804.appspot.com",
+//     messagingSenderId: "768186841488",
+//     appId: "1:768186841488:web:b452ddf681b75ee34a9b15",
+//     measurementId: "G-RRR5QKQKMF"
+//   };
+//   firebase.initializeApp(firebaseConfig);
+//   const db = firebase.firestore();
+// // 
 // 
 // 
 // 
@@ -28,4 +28,8 @@ const sendMessage = () => {
     let message1 = document.getElementById("message")
     message1.value = ""
     return false; 
+    
 }
+console.log("message",message)
+console.log("prompt",prompt)
+// console.log("db",db)
